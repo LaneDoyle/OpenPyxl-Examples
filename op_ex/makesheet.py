dd8a256d243bd0f647e4d421be69caa5b281a9e0
LaneDoyle <ldoyle@stcharlessd.org>
2019-12-12 13:16:02 -0600

    modified:   op_ex/example1.xlsx 	modified:   op_ex/makesheet.py

--- op_ex/makesheet.py
+++ op_ex/makesheet.py
@@ -16,3 +16,18 @@ sheet['B1'].value = "First Name"
 sheet['C1'].value = "Last Name"
 sheet['D1'].value = "Username"
 
+for i in range(6):
+    new_row = sheet.max_row+1
+    
+    first = input("Enter first name of new user: ")
+    last = input("Enter last name of new user: ")
+    u_name = input("Enter username of new user: ")
+    
+    print("-----------------------")#Breaks up loop
+    
+    sheet.cell(row = new_row, column = 1).value = dt.datetime.now()
+    sheet.cell(row = new_row, column = 2).vale = first
+    sheet.cell(row = new_row, column = 3).vale = last
+    sheet.cell(row = new_row, column = 4).vale = u_name
+    
+workbook.save("users.xlsx")
