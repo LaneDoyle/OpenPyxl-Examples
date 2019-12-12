b78901f8f934e1ae4ec7dd0d0bbe691b3a88e191
LaneDoyle <ldoyle@stcharlessd.org>
2019-12-12 13:38:55 -0600

    new file:   op_ex/.~lock.users.xlsx# 	modified:   op_ex/makesheet.py 	new file:   op_ex/users.xlsx

--- op_ex/makesheet.py
+++ op_ex/makesheet.py
@@ -7,27 +7,57 @@ import openpyxl
 import datetime as dt
 import time
 
-workbook = openpyxl.Workbook()
-sheet = workbook.create_sheet("Users")
-
-#Set the headings row
-sheet['A1'].value = "Timestamp"
-sheet['B1'].value = "First Name"
-sheet['C1'].value = "Last Name"
-sheet['D1'].value = "Username"
-
-for i in range(6):
-    new_row = sheet.max_row+1
+try:
+    print("Loading...")
+    workbook = openpyxl.load_workbook("users.xlsx")
+    sheet = workbook.get_sheet_by_name("Users")
     
-    first = input("Enter first name of new user: ")
-    last = input("Enter last name of new user: ")
-    u_name = input("Enter username of new user: ")
+    sheet['A1'].value = "Timestamp"
+    sheet['B1'].value = "First Name"
+    sheet['C1'].value = "Last Name"
+    sheet['D1'].value = "Username"
     
-    print("-----------------------")#Breaks up loop
+    for i in range(2):
+        new_row = sheet.max_row+1
+        
+        first = input("Enter first name of new user: ")
+        last = input("Enter last name of new user: ")
+        u_name = input("Enter username of new user: ")
+        
+        print("-----------------------")#Breaks up loop
+        
+        sheet.cell(row = new_row, column = 1).value = dt.datetime.now()
+        sheet.cell(row = new_row, column = 2).value = first
+        sheet.cell(row = new_row, column = 3).value = last
+        sheet.cell(row = new_row, column = 4).value = u_name
+        
+    workbook.save("users.xlsx")
+    print("Done!")    
     
-    sheet.cell(row = new_row, column = 1).value = dt.datetime.now()
-    sheet.cell(row = new_row, column = 2).vale = first
-    sheet.cell(row = new_row, column = 3).vale = last
-    sheet.cell(row = new_row, column = 4).vale = u_name
+except:
+    print("Failed to load...creating new sheet!")
+    workbook = openpyxl.Workbook()
+    sheet = workbook.create_sheet("Users")
     
-workbook.save("users.xlsx")
+    #Set the headings row
+    sheet['A1'].value = "Timestamp"
+    sheet['B1'].value = "First Name"
+    sheet['C1'].value = "Last Name"
+    sheet['D1'].value = "Username"
+    
+    for i in range(2):
+        new_row = sheet.max_row+1
+        
+        first = input("Enter first name of new user: ")
+        last = input("Enter last name of new user: ")
+        u_name = input("Enter username of new user: ")
+        
+        print("-----------------------")#Breaks up loop
+        
+        sheet.cell(row = new_row, column = 1).value = dt.datetime.now()
+        sheet.cell(row = new_row, column = 2).value = first
+        sheet.cell(row = new_row, column = 3).value = last
+        sheet.cell(row = new_row, column = 4).value = u_name
+        
+    workbook.save("users.xlsx")
+    print("Done!")
